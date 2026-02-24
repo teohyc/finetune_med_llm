f4e8aa0054f631b871bae238f6c74482507e1e1e
teohyc <teohyc2018@gmail.com>
2026-02-24 12:44:35 +0800

    first commit

---
+++ finetune_llama.py
@@ -0,0 +1,77 @@
+import torch
+import gc
+from datasets import load_dataset
+from unsloth import FastLanguageModel
+from transformers import TrainingArguments
+from trl import SFTTrainer
+
+model_name = "meta-llama/Llama-3.2-3B"
+max_seq_length = 1024
+dtype = torch.float16
+load_in_4bit = True
+
+#load model
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = model_name,
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit
+)
+
+#lora adapter
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 8,
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj"],
+    lora_alpha = 16,
+    lora_dropout = 0.05,
+    bias = "none",
+    use_gradient_checkpointing = True,
+)
+
+#load dataset
+dataset = load_dataset("json", data_files="med_training_data.json", split="train")
+
+def format_prompt(example):
+    return {
+        "text": f"""### Instruction:
+{example['instruction']}
+
+### Input:
+{example['input']}
+
+### Response:
+{example['output']}"""
+    }
+
+
+dataset = dataset.map(format_prompt)
+
+#training
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset= dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    args = TrainingArguments(
+        per_device_train_batch_size = 1,
+        gradient_accumulation_steps = 4, #batch size of 4
+        warmup_steps = 10,
+        max_steps = 300,
+        learning_rate = 1e-4,
+        fp16 = True,
+        logging_steps = 10,
+        output_dir = "outputs",
+        optim = "paged_adamw_8bit",
+        lr_scheduler_type = "cosine",
+        save_strategy = "steps",
+        save_steps = 100,
+    )
+)
+
+trainer.train()
+
+#save lora
+model.save_pretrained("lora_model")
+tokenizer.save_pretrained("lora_model")
---
+++ gguf_create.py
@@ -0,0 +1,18 @@
+import torch
+from unsloth import FastLanguageModel
+
+# load lora
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "lora_model", 
+    max_seq_length = 1024,
+    dtype = torch.float16,
+    load_in_4bit = True,       
+)
+
+#export to gguf
+model.save_pretrained_gguf(
+    "llama3.2_med_xray_model",            
+    tokenizer, 
+    quantization_method = "q4_k_m" 
+)
+print("GGUF export complete")
